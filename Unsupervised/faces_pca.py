75a2a6d83fe51c56c92abd3b6dd2a5dd9e771030
AmrKhalifa <amr.khalifa.m.a@gmail.com>
2020-01-15 00:23:43 +0200

    finished code to PCA on images

--- Unsupervised/faces_pca.py
+++ Unsupervised/faces_pca.py
@@ -1,10 +1,28 @@
-
 from sklearn.datasets import fetch_lfw_people
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-
-
-# #############################################################################
-# Download the data, if not already on disk and load it as numpy arrays
+import matplotlib.pyplot as plt 
+import numpy as np 
 
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+
+images = lfw_people.images
+b_size, H, W = images.shape
+
+plt.imshow(images[15])
+
+vectorized_images = images.reshape(-1, H* W)
+
+print(vectorized_images.shape)
+
+u, s, vt = np.linalg.svd(vectorized_images)
+
+print(u.shape)
+print(s.shape)
+print(vt.shape)
+
+# Projecting and reconstructing using only 50 principal components 
+f_5 = u[:,:50] @ np.diag(s[:50]) @ vt[:50]
+
+
+plt.figure()
+plt.imshow(f_5[15].reshape(H, W))
+plt.show()
