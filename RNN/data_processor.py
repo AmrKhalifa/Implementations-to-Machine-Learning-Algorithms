b0f4825db6d966c6a42256e3ba04767374def011
AmrKhalifa <amr.khalifa.m.a@gmail.com>
2020-02-12 00:12:00 +0200

    finished RNN for image classification

---
+++ RNN/data_processor.py
@@ -0,0 +1,21 @@
+
+import torch
+from torchvision import datasets, transforms
+
+
+train_set = datasets.MNIST(root='../data/', train=True, download=False, 
+	transform=transforms.Compose([
+            transforms.ToTensor()
+            ]))
+
+test_set = datasets.MNIST(root='../data/', train=False, download=False, 
+	transform=transforms.Compose([
+            transforms.ToTensor()
+            ]))
+
+def main():
+	pass 
+	
+if __name__ == "__main__":
+
+	main()
